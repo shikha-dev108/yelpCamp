df609ea17de5d1479a741c21fee8f4abe3a1570e
shikha-dev108 <skumari200298@gmail.com>
2024-10-31 11:58:35 +0530

    breaked the routes and Add express-flash

--- models/campground.js
+++ models/campground.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose');
+const Review = require('./review');
+const { campgroundSchema } = require('../schemas');
 const Schema = mongoose.Schema;
 
 const CampgroundSchema = new Schema({
@@ -6,9 +8,24 @@ const CampgroundSchema = new Schema({
     image: String,
     price: Number,
     description: String,
-    location: String
+    location: String,
+    reviews: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'Review'
+        }
+    ]
 });
+
+CampgroundSchema.post('findOneAndDelete', async function (doc) {
+    if (doc) {
+        await Review.deleteMany({
+            _id: {
+                $in: doc.reviews
+            }
+        })
+    }
+})
 
 module.exports = mongoose.model('Campground', CampgroundSchema);
 
-  
